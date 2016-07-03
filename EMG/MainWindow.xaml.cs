c3b0d4a53dc7b7247ab91e2dbf51532a5851bab2
Fantast <greefeet@hotmail.com>
2016-07-03 13:49:54 +0700

    Clean Code

--- EMG/MainWindow.xaml.cs
+++ EMG/MainWindow.xaml.cs
@@ -23,12 +23,38 @@ namespace EMG
         public MainWindow()
         {
             InitializeComponent();
+            ChageView(Page.Welcome, false);
         }
 
         private void Window_StateChanged(object sender, EventArgs e)
         {
             if (WindowState == System.Windows.WindowState.Maximized) Windows.Margin = new Thickness(7);
             else if (WindowState == System.Windows.WindowState.Normal) Windows.Margin = new Thickness(0, 0, 0, 0);
+        }
+
+        private void BtnConfig_Click(object sender, RoutedEventArgs e)
+        {
+            ChageView(Page.Config, true);
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            ChageView(Page.Config, true);
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            ChageView(Page.Run, true);
+        }
+        void ChageView(Page Goto,bool UseTranslate)
+        {
+            VisualStateManager.GoToElementState(Windows, Goto.ToString(), UseTranslate);
+        }
+        enum Page
+        {
+            Welcome,
+            Config,
+            Run
         }
     }
 }
